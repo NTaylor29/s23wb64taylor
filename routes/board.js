d08789b182873a99f8612174ff68f24461405d3d
Nicholas Taylor <70026110+NTaylor29@users.noreply.github.com>
2023-04-05 16:51:27 -0500

    Update board.js

--- routes/board.js
+++ routes/board.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("board", { title: "board", query: query});
   let query = req.query
+  res.render("board", { title: "board", query: query});
   console.log(`rows ${query.rows}`)
   console.log(`cols ${query.cols}`)
 });
