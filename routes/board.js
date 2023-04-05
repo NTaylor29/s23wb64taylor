413e9481ee25fdf8b2254dcea7959ed30689b8a6
Nicholas Taylor <70026110+NTaylor29@users.noreply.github.com>
2023-04-05 16:22:35 -0500

    trying to add query object

--- routes/board.js
+++ routes/board.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('board', { title: "Board Display"}  );
+router.get('/', function(req, { title: Board, query: query} , next) {
+  res.render("board", { title: "Board Display"}  );
   let query = req.query
   console.log(`rows ${query.rows}`)
   console.log(`cols ${query.cols}`)
